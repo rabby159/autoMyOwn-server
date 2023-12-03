64e78627c42ffa092bedc25b03bdaba2bf9bc7dd
Md Rabby <ablzerabbi159@gmail.com>
2023-12-04 03:01:59 +0600

    create api for all product delete and update

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch("/addProduct/:id", async (req, res) => {
+    app.patch("/addProduct/:id",verifyToken, async (req, res) => {
       const item = req.body;
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -169,6 +169,13 @@ async function run() {
       const result = await productCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
+
+    app.delete('/addProduct/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
